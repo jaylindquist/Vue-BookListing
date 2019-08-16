a51f4ab094e936147603d066ed8deb8f86b82b02
Jay Lindquist <jay.lindquist@target.com>
2019-08-16 07:37:37 -0500

    module2

--- src/components/BookList.vue
+++ src/components/BookList.vue
@@ -1,7 +1,10 @@
 <template>
-    <div>
-        <h1>{{ title }}</h1>
-    </div>
+  <div>
+    <h1>{{ title }}</h1>
+    <ul>
+      <li v-for="book in books" v-bind:key="book.title">{{ book.title }}:{{ book.author }}</li>
+    </ul>
+  </div>
 </template>
 
 <script>
@@ -9,14 +12,30 @@ export default {
     name: 'BookList',
     data() {
         return {
-            title: 'All Books'
+            title: 'All Books',
+            books: [
+                {title: 'Self-Reliance', author: 'Ralph Waldo Emerson'},
+                {title: 'American Gods', author: 'Neil Gaiman'},
+                {title: 'Amusing Ourselves to Death', author: 'Neil Postman'},
+            ],
         }
     }
 }
 </script>
 
 <style>
-h1, h2 {
-    font-weight: normal
+h1,
+h2 {
+  font-weight: normal;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: block;
+  margin: 0 10px;
 }
 </style>
